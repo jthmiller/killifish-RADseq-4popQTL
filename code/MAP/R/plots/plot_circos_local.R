153b185c175d14a8e2efa23068d17469e9b380fc
jtmiller <jtmiller@umn.edu>
2021-11-22 13:00:05 -0500

    process

--- code/MAP/R/plots/plot_circos_local.R
+++
@@ -1,7 +0,0 @@
-
-
-basedir <- "/Users/jeffreymiller/Documents/Projects/Killifish/QTL_agri"
-
-pop <- 'NBH'
-source("/Users/jeffreymiller/Documents/Projects/Killifish/QTL_agri/MAP/R/control_file.R")
-mpath <- '/Users/jeffreymiller/Documents/Projects/Killifish/QTL_agri/data'
--- code/seq/04_process.variants.sh
+++ code/seq/04_process.variants.sh
@@ -3,18 +3,11 @@
 module load vcftools/0.1.13
 vcftools --version
 
+set -o allexport
+source code/seq/analysis.env
+set +o allexport
 
-## vcftools filters
-## $maf $maxf
-
-for X in NBH BRP NEW
-do
-	#vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtered-all --recode --stdout | vcftools --vcf - --max-meanDP 90 --maxDP 90 --stdout --minGQ 20 $maf $maxf --remove-filtered-all --recode | gzip -c > $vcfdir/$X.vcf.gz
-	$plink --vcf $vcfdir/$X.vcf.gz --out $indpops/$X  $init_flagset --pheno $pheno --all-pheno --update-ids $infiles/SOMM.txt
-	$plink --bfile $indpops/$X --out $indpops/$X $flagset --make-bed --pheno $pheno --all-pheno --keep-cluster-names $X --make-founders
-	$plink --bfile $indpops/$X --out $indpops/$X $flagset --pheno $pheno --all-pheno --keep-cluster-names $X $geno $maf --recode --biallelic-only strict --snps-only just-acgt --nonfounders
-done
-
+### Output allele counts to explore data in R
 for X in NBH BRP NEW ELR
 do
 vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtered-all --recode --stdout \
@@ -23,13 +16,7 @@ vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtere
 | vcftools --vcf - --counts > ~/QTL_agri/data/$X.counts
 done
 
-for X in NBH BRP NEW ELR
-do
-vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtered-all --recode --stdout \
-| vcftools --vcf - --max-meanDP 90 --maxDP 90 --stdout --minGQ 20 $maf --remove-filtered-all --recode --stdout \
-| > ~/QTL_agri/data/$X.vcf
-done
-
+### Output site mean depth to explore data in R
 for X in NBH BRP NEW ELR
 do
 vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtered-all --recode --stdout \
@@ -38,9 +25,21 @@ vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtere
 | vcftools --vcf - --site-mean-depth > ~/QTL_agri/data/$X.site-mean-depth
 done
 
+### Filter
+for X in NBH BRP NEW ELR
+do
+vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtered-all --recode --stdout \
+| vcftools --vcf - --max-meanDP 90 --maxDP 90 --stdout --minGQ 20 $maf --remove-filtered-all --recode --stdout \
+| gzip -c > $vcfdir/$X.vcf.gz> ~/QTL_agri/data/$X.vcf
+done
 
-
-
+## Use PLINK to seperate familes for rQTL. Alignment quality drops off for loci that have mean depth over 90. 
+for X in NBH BRP NEW
+do
+	$plink --vcf $vcfdir/$X.vcf.gz --out $indpops/$X  $init_flagset --pheno $pheno --all-pheno --update-ids $infiles/SOMM.txt
+	$plink --bfile $indpops/$X --out $indpops/$X $flagset --make-bed --pheno $pheno --all-pheno --keep-cluster-names $X --make-founders
+	$plink --bfile $indpops/$X --out $indpops/$X $flagset --pheno $pheno --all-pheno --keep-cluster-names $X $geno $maf --recode --biallelic-only strict --snps-only just-acgt --nonfounders
+done
 
 ### REFILTER ELR. It contains the parent 'BLI' code (in addition to ELR fam)
 $plink --vcf $vcfdir/SOMM.vcf.gz --out $indpops/$X  $init_flagset --make-bed --pheno $pheno --all-pheno --update-ids $infiles/SOMM.txt
@@ -70,13 +69,3 @@ do
 	$plink --bfile $indpops/ALL --out $indpops/$i.parents --make-bed --chr $i --set-missing-var-ids @:# --allow-extra-chr --autosome-num 24 --allow-no-sex --pheno $pheno --all-pheno --keep $infiles/parents.txt --keep-cluster-names NEW NBH ELR BLI --recode --family --snps-only just-acgt --nonfounders --distance square bin 1-ibs ibs flat-missing
 done
 
-#grab unmapped data
-X=NBH
-#vcftools --gzvcf $vcfdir/SOMM.vcf.gz --keep $infiles/$X.samples --remove-filtered-all --recode --stdout | vcftools --vcf - --stdout --minGQ 20 $maf $maxf --remove-filtered-all --recode | gzip -c > $vcfdir/$X.uf.vcf.gz
-#$plink --vcf $indpops/$X.um.vcf.gz --out $indpops/$X.um --make-bed --autosome-num 24 --allow-extra-chr --allow-no-sex --pheno $pheno --all-pheno --update-ids $infiles/SOMM.txt --make-founders
-
-
-
-$plink --vcf ~/QTL_Map_Raw/vcf/freebayes.array/SOMM.bgzip.vcf.gz --out $indpops/$X.um --make-bed --autosome-num 24 --allow-extra-chr --allow-no-sex --pheno $pheno --all-pheno --update-ids $infiles/SOMM.txt --make-founders
-
-$plink --bfile $indpops/$X.um --out $indpops/$X.um  --recode --autosome-num 24 --allow-no-sex  --allow-extra-chr --family --set-missing-var-ids @:# --pheno $pheno --all-pheno --keep-cluster-names $X --make-founders  $geno $maf  --biallelic-only strict --snps-only just-acgt --nonfounders
